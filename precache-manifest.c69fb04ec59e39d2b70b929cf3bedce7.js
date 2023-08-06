ae9316db406fa88f7a4ec2c1d45098cad3493b98
jamessnader4444 <138589695+jamessnader4444@users.noreply.github.com>
2023-08-05 18:02:38 -0700

    init

---
+++ precache-manifest.c69fb04ec59e39d2b70b929cf3bedce7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e3c00cc93be1a85a4a1f6b84e84346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ddb4e047cbc4dcd7f9",
+    "url": "/static/css/2.83a79368.chunk.css"
+  },
+  {
+    "revision": "6b0eb405c0e97491a1aa",
+    "url": "/static/css/main.b2e7b877.chunk.css"
+  },
+  {
+    "revision": "91ddb4e047cbc4dcd7f9",
+    "url": "/static/js/2.b155e06d.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "/static/js/2.b155e06d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0eb405c0e97491a1aa",
+    "url": "/static/js/main.d25f815e.chunk.js"
+  },
+  {
+    "revision": "ecc8794fefe937d5093b",
+    "url": "/static/js/runtime-main.8dccfd95.js"
+  },
+  {
+    "revision": "3d0ec57f40d62ba6a8e0d1857bb8f7d5",
+    "url": "/static/media/bootstrap-icons.3d0ec57f.woff"
+  },
+  {
+    "revision": "a30fb81bd52143bcd4de2898422ac8b9",
+    "url": "/static/media/bootstrap-icons.a30fb81b.woff2"
+  }
+]);
